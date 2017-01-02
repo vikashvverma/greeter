e272760da54026fe867cac515865d260ad843234
vikashkv <vikashkv@invikashkv.local>
2017-01-02 13:22:50 +0530

    Add time as config

--- config/config.go
+++ config/config.go
@@ -3,7 +3,10 @@ package config
 import "github.com/sendgrid/sendgrid-go/helpers/mail"
 
 type Config struct {
-	APIKey  string
+	Time string
+
+	APIKey string
+
 	From    *mail.Email
 	Subject string
 	To      *mail.Email
@@ -15,10 +18,11 @@ func New() Config {
 	t := mail.NewEmail("Vikash", "vermavikash014@gmail.com")
 	c := mail.NewContent("text/plain", "Many happy returns of the day!")
 	return Config{
-		APIKey: "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",
-		From:   f,
-		Subject:"Happy B'day!",
-		To:     t,
-		Content:c,
+		Time:    "13:04",
+		APIKey:  "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",
+		From:    f,
+		Subject: "Happy B'day!",
+		To:      t,
+		Content: c,
 	}
 }
