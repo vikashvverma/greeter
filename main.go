98f3a53525c517b29a73ec8e2ed4775568bc2d24
vikashkv <vikashkv@invikashkv.local>
2017-01-03 19:20:51 +0530

    Don't start server

--- main.go
+++ main.go
@@ -1,18 +1,12 @@
 package main
 
 import (
-	"fmt"
 	"log"
-	"net/http"
-	"os"
-
+	
 	"github.com/vikashvverma/greeter/config"
 	"github.com/vikashvverma/greeter/job"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
-}
 func main() {
 	c := config.ReadConfig("./config.json")
 	if c == nil {
@@ -25,12 +19,4 @@ func main() {
 		log.Fatalf("ListenAndServe: %s", err)
 	}
 	<-gocron.Start()
-
-	// No need of a server, a server is just to check the app status easily on cloud
-	port := os.Getenv("PORT")
-	if port == "" {
-		log.Fatal("$PORT must be set")
-	}
-	http.HandleFunc("/", handler)
-	http.ListenAndServe(fmt.Sprintf(":%s", port), nil)
 }
